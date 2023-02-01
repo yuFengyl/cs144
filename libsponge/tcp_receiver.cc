9740e429fbe904a6de29c124be6f21b21055bc15
yuFeng <3200105721@zju.edu.cn>
2023-02-01 10:02:57 +0800

    lab2 passed

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,28 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    TCPHeader header = seg.header();
+    if (_syn_flag == false){
+        if (header.syn == false)
+            return ;
+        _syn_flag = true;
+        _isn = header.seqno;
+    }
+    string data = seg.payload().copy();
+    _check_point = unwrap(header.seqno, _isn, _check_point);
+    // 这里当 header.syn 为 true 时也要占据一个 seqno ，因此有下面的判断，并且要加一
+    uint64_t index = header.syn ? _check_point : _check_point - 1;
+    _reassembler.push_substring(data, index, header.fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (_syn_flag == false)
+        return nullopt;
+    uint64_t abs_ackno = _reassembler.stream_out().bytes_written() + 1;
+    // 这里当 header.fin 为 true 时也要占据一个 seqno ，因此有下面的判断，并且要加一
+    if (_reassembler.stream_out().input_ended() == true)
+        abs_ackno++;
+    return _isn + abs_ackno; // add the syn
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return stream_out().remaining_capacity(); }
